c16be9bce37e2e5718f743c49f7ff54bf6028d44
Kevin Nalinne <kevin@kvnl.be>
2025-04-02 15:41:25 +0200

    separate endpoints

---
+++ Customers/CustomerEndpoints.cs
@@ -0,0 +1,51 @@
+namespace webapi.Customers;
+
+public static class CustomerEndpoints
+{
+    public static void MapCustomerEndpoints(this WebApplication app)
+    {
+        app.MapGet("/customers", async (CustomerService service) =>
+        {
+            var customers = await service.List();
+            return Results.Ok(customers);
+        });
+
+        app.MapPost("/customers",
+            async (Customer request, CustomerService service) =>
+            {
+                var newCustomer = new Customer(request.Id, request.Name);
+                await service.Add(newCustomer);
+                return Results.Created($"/customers/{newCustomer.Id}", newCustomer);
+            });
+
+        app.MapPut("/customers/{id}", async (int id, Customer request, CustomerService data) =>
+        {
+            var existingCustomer = await data.GetById(id);
+            if (existingCustomer is null)
+            {
+                return Results.NotFound();
+            }
+
+            var updatedCustomer = existingCustomer with
+            {
+                Name = request.Name
+            };
+            await data.Update(id, updatedCustomer);
+            return Results.Ok(updatedCustomer);
+        });
+
+
+        app.MapDelete("/user/{id}", async (int id, CustomerService data) =>
+        {
+            var customer = await data.GetById(id);
+            if (customer is null)
+            {
+                return Results.NotFound();
+            }
+
+            await data.DeleteById(id);
+
+            return Results.NoContent();
+        });
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using webapi.Customers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<CustomerService>();
 
 var app = builder.Build();
 
@@ -18,39 +20,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-List<Customer> customers = new List<Customer>();
-
-app.MapGet("/customers", () =>
-{
-    return Results.Ok(customers);
-});
-
-app.MapPost("/customers", (Customer customer) =>
-{
-    customers.Add(customer);
-    return Results.Ok();
-});
-
-app.MapPut("/customers/{id}", (int id, Customer customer) =>
-{
-    var existing = customers.FirstOrDefault(c => c.id == id);
-    if (existing == null)
-        return Results.BadRequest("id not found");
-    customers.Remove(existing);
-    customers.Add(customer);
-    return Results.Ok();
-});
-
-
-app.MapDelete("/user/{id}", (int id) =>
-{
-    var existing = customers.FirstOrDefault(c => c.id == id);
-    if (existing == null)
-        return Results.BadRequest("id not found");
-    customers.Remove(existing);
-    return Results.Ok();
-});
+app.MapCustomerEndpoints();
 
 await app.RunAsync();
-
-record Customer(int id, string name);
