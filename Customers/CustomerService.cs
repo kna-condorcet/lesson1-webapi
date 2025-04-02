84523e98731a4536378ba2d5d5ae2f1276a80a19
Kevin Nalinne <kevin@kvnl.be>
2025-04-02 16:01:29 +0200

    refacto

---
+++ Customers/CustomerService.cs
@@ -0,0 +1,55 @@
+namespace webapi.Customers;
+
+public class CustomerService
+{
+    private static readonly List<Customer> _customers = new();
+    
+    public CustomerService()
+    {
+        _customers.AddRange(new []
+        {
+            new Customer(1, "Acme"),
+            new Customer(2, "Contoso"),
+            new Customer(3, "WebstanZ"),
+        });
+    }
+    
+    public Task<IEnumerable<Customer>> List()
+    {
+        return Task.FromResult(_customers.ToArray().AsEnumerable());
+    }
+    
+    public Task<Customer> GetById(int customerId)
+    {
+        var customer = _customers.FirstOrDefault(c => c.Id == customerId);
+        return Task.FromResult(customer);
+    }
+    
+    public Task Add(Customer customer)
+    {
+        _customers.Add(customer);
+        return Task.CompletedTask;
+    }
+
+    public Task Update(int id, Customer updatedCustomer)
+    {
+        if (_customers.Any(c => c.Id == id))
+        {
+            var index = _customers.FindIndex(c => c.Id == id);
+            _customers[index] = updatedCustomer;
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteById(int id)
+    {
+        if (_customers.Any(c => c.Id == id))
+        {
+            var index = _customers.FindIndex(c => c.Id == id);
+            _customers.RemoveAt(index);
+        }
+        return Task.CompletedTask;
+    }
+}
+
+public record Customer(int Id, string Name);
